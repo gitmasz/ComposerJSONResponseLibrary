4e0479cbc883c90627480fcae8a47da76cbb5600
Marcin Szczepkowski <m.szczepkowski@hotmail.com>
2024-01-24 20:40:35 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once __DIR__ . "/vendor/autoload.php";
+
+use Devscreencast\ResponseClass\JsonResponse;
+
+$student = array(
+    'name' => 'John Doe',
+    'course' => 'Software Engineering',
+    'level' => '200',
+    'collections' => ['books' => 'Intro to UML', 'music' => 'rap']
+);
+
+header('Content-Type: application/json');
+
+new JsonResponse('ok', '', $student);
